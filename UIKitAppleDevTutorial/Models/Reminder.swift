b03f86567e5b439b23b998ab7f1aedbffa3e4c87
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-09-21 23:16:04 +0300

    Create a new cell registration. Retrieve the reminder corresponding to the item. Retrieve the cell’s default content configuration. Assign reminder.title to the content configuration text. Assign the content configuration to the cell. Add a type alias for the diffable data source. Add a dataSource property that implicitly unwraps a DataSource. Create a new data source. Dequeue and return a cell using the cell registration.

--- UIKitAppleDevTutorial/Models/Reminder.swift
+++ UIKitAppleDevTutorial/Models/Reminder.swift
@@ -17,7 +17,7 @@ struct Reminder {
 
 #if DEBUG
 extension Reminder {
-    static var reminderData = [
+    static var sampleData = [
         Reminder(title: "Submit reimbursement report", dueDate: Date().addingTimeInterval(800.0), notes: "Don't forget about taxi receipts"),
         Reminder(title: "Code review", dueDate: Date().addingTimeInterval(14000.0), notes: "Check tech specs in shared folder", isComplete: true),
         Reminder(title: "Pick up new contacts", dueDate: Date().addingTimeInterval(24000.0), notes: "Optometrist closes at 6:00PM"),
--- UIKitAppleDevTutorial/ReminderListViewController.swift
+++ UIKitAppleDevTutorial/ReminderListViewController.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let listLayout = listLayout()
         collectionView?.collectionViewLayout = listLayout
-        // Do any additional setup after loading the view.
+        
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, identifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
     }
     
     private func listLayout() -> UICollectionViewCompositionalLayout {
